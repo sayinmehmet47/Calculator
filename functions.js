85618f29791365fcf5c0b8606d3ebb56c2e1cbe1
sayin47 <sayinmehmet47@gmail.com>
2021-04-01 17:11:12 +0300

    operators are functional

--- functions.js
+++ functions.js
@@ -1,23 +1,39 @@
-
 class Calculator {
-    constructor(previous,current) {
-      this.previous = previous;
-      this.current = current;
-    }
-  
-  
-    clear() {
-        
-        $(".result").text("0")
-        previous=0;
-        
-
-    }
-  
-  
-  
-  
-  
+  constructor(previous, current, text) {
+    this.previous = previous;
+    this.current = current;
+    this.text = text;
   }
-  
- 
+
+  clear() {
+    $('.result').text('0');
+    $('.previous').text('0');
+
+    previous = 0;
+  }
+
+  confirmation() {
+    let arr = $('.result').text().split('');
+    let last = arr[arr.length - 1];
+    console.log(last === '*');
+    if (
+      last === '*' ||
+      last === '+' ||
+      last === '/' ||
+      last === '-' ||
+      last === '='
+    ) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  display() {
+    console.log(text);
+    $('.result').text(eval(text));
+    $('.previous').text(text);
+  }
+}
+
+const ui = new Calculator(previous, current, text);
--- script.js
+++ script.js
@@ -1,40 +1,55 @@
 var value;
-var previous=Number($(".result").text());
-var current=0;
-
-
-
+var previous = Number($('.result').text());
+var text;
+var current = 0;
 
 $('.num').on('click', function (e) {
   value = Number($(this).val());
 
-  if(previous==0){
-
-   previous=Number($(".result").text(`${value}`).text())
-   console.log(previous)
+  if (previous == 0) {
+    previous = Number($('.result').text(`${value}`).text());
+  } else {
+    $('.result').append(`${value}`);
+    previous = Number($('.result').text());
+    text=$('.result').text()
+    
    
-
-  }else{
-    $(".result").append(`${value}`)
-    previous=Number($(".result").text())
-   
-    console.log(previous)
+    console.log(eval(text))
 
   }
-
-  
 
   e.preventDefault();
 });
 
 
+
 //clearing the screen
-$("#clear").on("click",function(e){
+$('#clear').on('click', function (e) {
+  ui.clear();
+});
+//adding operators
+$('.operator').on('click', function (e) {
+  const operator = $(this).val();
 
-  const ui=new Calculator(previous,current)
-  ui.clear()
+  if(ui.confirmation()){
+
+    $('.result').append(operator);
+    text=$(".result").text()
+
+  }
+
+  e.preventDefault();
+});
+
+
+// when clicking on equal
+$('#equals').on('click', function (e) {
+ 
+  ui.display()
+
+
+  e.preventDefault();
+});
 
 
 
-
-})
